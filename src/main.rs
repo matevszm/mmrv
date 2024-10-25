25189f08a29404b0482cf97c10daaacf9c71c910
matevszm <mateusz.marcinkowski@blockydevs.com>
2024-10-25 14:11:52 +0200

    compile wip

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use ckb_merkle_mountain_range;
 use ckb_merkle_mountain_range::{Error, Merge, MMR, Result};
 use ckb_merkle_mountain_range::util::MemStore;
@@ -11,7 +10,13 @@ fn new_blake2b() -> Blake2b {
 }
 
 #[derive(Eq, PartialEq, Clone, Debug, Default)]
-struct NumberHash(pub Bytes);
+struct Version {
+    hash: Bytes,
+    version: u32
+}
+
+#[derive(Eq, PartialEq, Clone, Debug, Default)]
+struct NumberHash(pub Vec<Version>);
 impl TryFrom<u32> for NumberHash {
     type Error = Error;
     fn try_from(num: u32) -> Result<Self> {
@@ -19,7 +24,10 @@ impl TryFrom<u32> for NumberHash {
         let mut hash = [0u8; 32];
         hasher.update(&num.to_le_bytes());
         hasher.finalize(&mut hash);
-        Ok(NumberHash(hash.to_vec().into()))
+        Ok(NumberHash(vec![Version{
+            version: 0,
+            hash: hash.to_vec().into()
+        }]))
     }
 }
 
@@ -30,16 +38,14 @@ impl Merge for MergeNumberHash {
     fn merge(lhs: &Self::Item, rhs: &Self::Item) -> Result<Self::Item> {
         let mut hasher = new_blake2b();
         let mut hash = [0u8; 32];
-        hasher.update(&lhs.0);
-        hasher.update(&rhs.0);
+        hasher.update(&lhs.0.last().unwrap().hash);
+        hasher.update(&rhs.0.last().unwrap().hash);
         hasher.finalize(&mut hash);
-        Ok(NumberHash(hash.to_vec().into()))
+        Ok(NumberHash(vec![Version{
+            version: 0,
+            hash: hash.to_vec().into()
+        }]))
     }
-}
-
-struct MMRV<V, M> {
-    versions: HashMap<NumberHash, (NumberHash, NumberHash)>,
-    current: MMR::<V, M, V>
 }
 
 fn main() {
